17c34f404026d7aeed77308c334b57dd12030195
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-03 18:36:33 +0530

    Add User interface and update Prisma schema; configure static file serving

---
+++ server/types/user.d.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    fullname: string;
+    username: string;
+    email: string;
+    password: string;
+    profilephoto?: string;
+    gender: 'MALE' | 'FEMALE' | 'OTHER';
+}
