7948c7d99d7f530c72bdb93c3f401f6fa8468077
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-22 16:40:58 +0530

    Add JWT authentication to signin controller and update package dependencies

--- server/controllers/signin.controller.ts
+++ server/controllers/signin.controller.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { LoginRequest } from "../types/loginRequest.js";
 import { loginSchema } from "../schemas/login.schema.js";
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 export const signin = async (req: LoginRequest, res: Response) => {
@@ -42,9 +43,24 @@ export const signin = async (req: LoginRequest, res: Response) => {
       return;
     }
 
-    res.status(200).json({
+    const tokenData = {
+      userId: user.id,
+    }
+
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error("JWT_SECRET_KEY is not defined");
+    }
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {expiresIn: "1d"});
+    
+
+    res.status(200).cookie("token", token, {maxAge: 1*24*60*60*1000, httpOnly: true, sameSite: 'strict'}).
+    json({
       message: "User logged in successfully",
-      user,
+      // user, --> this is not safe to send to the client kyuki fir vo user ka password dekh skta hai
+      id: user.id,
+      username: user.username,
+      fullname: user.fullname,
+      profilephoto: user.profilephoto,
     });
 
     return;
