13df1938c9641ef67b9d2016bc7b94e761432325
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-23 18:19:03 +0530

    Enhance getOtherUser controller to retrieve all users except the logged-in user

--- server/controllers/getOtherUser.controller.ts
+++ server/controllers/getOtherUser.controller.ts
@@ -1,13 +1,31 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export const getOtherUser = async (req: Request, res: Response) => {
-try {
+  try {
     const loggedInUserId = req.body.userId;
-} catch (error) {
+    const otherUsers = await prisma.user.findMany({
+      where: {
+        NOT: {
+          id: loggedInUserId,
+        },
+      },
+      select: {
+        id: true,
+        username: true,
+        fullname: true,
+        profilephoto: true,
+        email: true,
+        gender: true,
+      },
+    });
+
+    res.status(200).json(otherUsers);
+  } catch (error) {
     console.error(error);
     res.status(500).json({
-        message: "Internal Server Error"
-    })
-    
-}
-}
+      message: "Internal Server Error",
+    });
+  }
+};
