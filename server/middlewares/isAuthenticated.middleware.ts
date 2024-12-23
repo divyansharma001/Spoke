813d43a985d9394f4f6b72a4ce99f63581391f54
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-23 17:53:22 +0530

    Add JWT verification to isAuthenticated middleware for enhanced security

--- server/middlewares/isAuthenticated.middleware.ts
+++ server/middlewares/isAuthenticated.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
     try{
@@ -11,7 +12,25 @@ const isAuthenticated = async (req: Request, res: Response, next: NextFunction) 
             return;
      }
 
+     if(!process.env.JWT_SECRET_KEY){
+        res.status(500).json({
+            message: "Internal Server Error, Env not found"
+        })
+        return;
+     }
 
+     const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY) as jwt.JwtPayload;
+
+     if(!decode){
+        res.status(401).json({
+            message: "User is not authenticated"
+        })
+        return;
+     }
+
+      req.body.id = (decode as jwt.JwtPayload).userId;  //this makes the id available in the req.body
+
+     next();
 
     }catch(error){
         console.error(error);
