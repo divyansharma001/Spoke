bbb6111095ff6ef63027dd00068f25e77385b29d
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-24 18:03:45 +0530

    Add helmet middleware for enhanced security

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
+import helmet from "helmet";
 import IndexRouter from './routes/index.route.js';
-
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(helmet());
 
 app.use(express.static('public'));
 
