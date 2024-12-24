0abaf8faa73125c31e0b77a8f388d5d95c15d67a
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-24 18:40:54 +0530

    Complete message controller. socket implementation is remaining

--- server/controllers/message.controller.ts
+++ server/controllers/message.controller.ts
@@ -3,44 +3,52 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const sendMessage = async(req: Request, res: Response) => {
-try {
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
     const sender_id = +req.body.id;
-    const receiver_id = +req.params.id
-    const {message} = req.body;
+    const receiver_id = +req.params.id;
+    const { message } = req.body;
 
     let gotConversation = await prisma.conversation.findFirst({
-        where: {
-          participants: {
-            hasEvery: [sender_id, receiver_id] 
-          }
+      where: {
+        participants: {
+          hasEvery: [sender_id, receiver_id],
         },
-        select: {
-          participants: true
-        }
-      });
+      },
+      include: {
+        messages: true,
+      },
+    });
 
-      if (!gotConversation) {
-        gotConversation = await prisma.conversation.create({
-          data: {
-            participants: [sender_id, receiver_id]
-          }
-        });
-      }
-
-      const newMessage = await prisma.message.create({
+    if (!gotConversation) {
+      gotConversation = await prisma.conversation.create({
         data: {
-          receiver_id,
-          sender_id,
-          message
-        }
-      })
-      
-} catch (error) {
+          participants: [sender_id, receiver_id],
+        },
+        include: {
+          messages: true,
+        },
+      });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        receiver_id,
+        sender_id,
+        message,
+      },
+    });
+
+    console.log(gotConversation);
+
+    if (newMessage) {
+      gotConversation.messages.push(newMessage)
+    }
+
+  } catch (error) {
     console.error(error);
     res.status(500).json({
-        message: "Internal Server Error"
-    })
-    
-}
-}
+      message: "Internal Server Error",
+    });
+  }
+};
