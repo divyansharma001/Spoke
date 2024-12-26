d10be6079610375c38cd620bf35ef75e1ac1a6cd
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-26 21:29:17 +0530

    Fix typo in logout success message

--- server/controllers/logout.controller.ts
+++ server/controllers/logout.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 export const logout = (req: Request, res: Response) => {
 try {
     res.status(200).cookie("token", "", {maxAge: 0, httpOnly: true, sameSite: 'strict'}).json({
-        message: "User logged out successfully"
+        message: "User logged out successfully !"
     })
     return;
 } catch (error) {
