f015c2732c7ca1535d47fe88329c98f568c475cd
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-27 19:29:12 +0530

    Add Spotlight component to main render for enhanced UI visibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import Navbar from './components/Navbar.tsx'
 import Footer from './components/Footer.tsx'
+import { Spotlight } from './components/ui/Spotlight.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="#ffffea"
+      />
     <Navbar/>
     <App />
     <Footer/>
