859d03a5321c2f9684614f4be316deeb05e9f35b
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-08 01:05:45 +0530

    Update react-day-picker to version 9.5.0 and clean up Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,12 @@
 import logo from "@/assets/talk.png";
 import Image from "next/image";
 import { Spotlight } from "./ui/Spotlight";
-import ShinyButton from "./ui/shiny-button";
-import { Inter } from "next/font/google";
 import InteractiveHoverButton from "./ui/interactive-hover-button";
-
 
 const Navbar = () => {
   return (
     <>
-      <div className=" bg-black text-white flex justify-between p-16 pt-9 bg-grid-small-white/[0.2]">
+      <div className="bg-black text-white flex justify-between p-16 pt-9 bg-grid-small-white/[0.2]">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="#2979FF"
@@ -26,14 +23,6 @@ const Navbar = () => {
           </div>
           <div className="text-xl font-semibold">Sp<span className="underline underline-offset-2 decoration-solid decoration-[#8ec4ff]">oke</span></div>
         </div>
-        {/* <div className="flex items-center space-x-8 pr-10">
-          <div>
-          <ShinyButton>Login</ShinyButton>
-          </div>
-          <div>
-          <ShinyButton>Signup</ShinyButton>
-          </div>
-        </div> */}
         <InteractiveHoverButton/>
       </div>
     </>
