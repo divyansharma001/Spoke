cb146e77c9d9b76d2407f4b6e12d5e073f838453
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-08 01:55:11 +0530

    Made Navbar responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,24 +6,27 @@ import InteractiveHoverButton from "./ui/interactive-hover-button";
 const Navbar = () => {
   return (
     <>
-      <div className="bg-black text-white flex justify-between p-16 pt-9 bg-grid-small-white/[0.2]">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="#2979FF"
-      />
-        <div className="pl-10 flex items-center space-x-1">
+      <div className="bg-black text-white flex justify-between p-4 sm:p-8 md:p-16 md:pt-9 bg-grid-small-white/[0.2]">
+        <Spotlight
+          className="-top-32 left-96 "
+          fill="#2979FF"
+        />
+        <div className="flex items-center space-x-1 pl-2 sm:pl-6 md:pl-10">
           <div className="text-white">
             <Image
               alt="logo"
               src={logo}
-              height={30}
-              width={30}
+              height={24}
+              width={24}
+              className="sm:h-[28px] sm:w-[28px] md:h-[30px] md:w-[30px]"
               style={{ filter: "invert(1) brightness(2)" }}
             />
           </div>
-          <div className="text-xl font-semibold">Sp<span className="underline underline-offset-2 decoration-solid decoration-[#8ec4ff]">oke</span></div>
+          <div className="text-lg sm:text-xl font-semibold">
+            Sp<span className="underline underline-offset-2 decoration-solid decoration-[#8ec4ff]">oke</span>
+          </div>
         </div>
-        <InteractiveHoverButton/>
+        <InteractiveHoverButton />
       </div>
     </>
   );
