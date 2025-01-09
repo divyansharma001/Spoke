0381089ba362054d515ce0047e212ae1f4c35f18
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-09 21:36:29 +0530

    Remove unnecessary "use client" directives from multiple components

--- client/src/pages/Features.tsx
+++ client/src/pages/Features.tsx
@@ -1,4 +1,3 @@
-'use client'
 
 import NotificationFeature from "@/components/NotificationFeature";
 import SaveFiles from "@/components/SaveFiles";
@@ -7,10 +6,8 @@ import SaveFiles from "@/components/SaveFiles";
 const Features = () => {
   return (
     <>
- 
    <NotificationFeature/>
    <SaveFiles/>
-   
    </>
   );
 };
