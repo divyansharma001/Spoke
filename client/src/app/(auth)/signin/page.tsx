62e3e91a9437c84ad7cb5d2b114af125c564f5fd
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-19 10:54:25 +0530

    Add LoginForm component and integrate it into SigninPage

--- client/src/app/(auth)/signin/page.tsx
+++ client/src/app/(auth)/signin/page.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
 export default function SigninPage(){
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            
+            <LoginForm/>
         </div>
     )
 }
